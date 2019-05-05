39a3a03196bcef716a085ec20961dc56e798a96c
chengz3906 <cehngz3906@gmail.com>
2019-05-04 21:52:22 -0400

    update eval

--- show_results.py
+++ show_results.py
@@ -3,23 +3,28 @@ import json
 hypo_path = '../results/coco_val2014_HYPO_max_cap_100_min_word_freq_3.json'
 refer_path = '../results/coco_val2014_REFER_max_cap_100_min_word_freq_3.json'
 
-hypo_b_path = '../results/coco_val2014_HYPO_max_cap_100_min_word_freq_3.json'
-refer_b_path = '../results/coco_val2014_REFER_max_cap_100_min_word_freq_3.json'
-
+hypo_b_path = '../results/coco_val2014_baseline_HYPO_max_cap_100_min_word_freq_3.json'
+result_txt = "../results/result.txt"
 
 def main():
     with open(hypo_path, 'r') as j:
         hypo_dict = json.load(j)
 
+    with open(hypo_b_path, 'r') as j:
+        hypo_b_dict = json.load(j)
+
     with open(refer_path, 'r') as j:
         refer_dict = json.load(j)
 
-    for img in hypo_dict:
-        hypo = hypo_dict[img]
-        refer = refer_dict[img]
-        print(img)
-        print(hypo)
-        print(refer)
+    with open(result_txt, 'w') as r:
+        for img in hypo_dict:
+            hypo = hypo_dict[img]
+            hypo_b = hypo_b_dict[img]
+            refer = refer_dict[img]
+            r.write("image name: %s\n" % img)
+            r.write("model: %s\n" % " ".join(hypo))
+            r.write("baseline: %s\n" % " ".join(hypo_b))
+            r.write("ground truth: %s\n\n" % " ".join(refer[0]))
 
 
 if __name__ == '__main__':
